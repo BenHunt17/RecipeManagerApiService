659e183f896baa463faa63767d8f9009e5a047c8
BenHunt17 <benlewishunt@gmail.com>
2022-02-17 22:33:10 +0000

    ASP.NET core web API project

---
+++ RecipeSchedulerApiService/RecipeSchedulerApiService/Controllers/RecipesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipeSchedulerApiService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RecipesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/recipes")]
+        public IActionResult Get()
+        {
+            return Ok(new List<string> { "beans on toast", "bangers and mash", "porridge" });
+        }
+    }
+}
